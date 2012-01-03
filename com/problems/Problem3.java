0981cc6a336e65a862ebddda3d89a54f6573dc65
Ritesh Agrawal <ragrawal@gmail.com>
2012-01-03 15:59:57 -0800

    added template and problems

---
+++ com/problems/Problem3.java
@@ -0,0 +1,88 @@
+package com.problems;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import com.algo.sort.BubbleSort;
+import java.util.Arrays;
+
+public class Problem3 extends Problem{
+	public Problem3(){
+		super();
+		this.solutions.add(new Solution1());
+	}
+
+	public String question(){
+		return "Write a method to decide if two strings are anagrams or not";
+	}
+	
+	public Map readParameters() throws Exception{
+		Map<String, String> options = new HashMap<String, String>();
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(isr);
+
+		System.out.print("Enter String 1: ");
+		options.put("str1", reader.readLine());
+
+		System.out.print("Enter String 2: ");
+		options.put("str2", reader.readLine());
+		
+	
+		return options;
+		
+	}
+	
+	public class Solution1 implements Solution{
+		
+		private String process(String str){
+			str = str.toLowerCase().trim();
+			return str.replaceAll("[^a-zA-Z0-9]", "");
+			
+		}
+		public void execute(Map options){
+			
+			String[] str1 = process((String) options.get("str1")).split("");
+			String[] str2 = process((String) options.get("str2")).split("");
+			
+			if(str1.length != str2.length ){
+				System.out.println("Two strings are not anagrams");
+				return;
+			}
+			
+			//Sort two strings
+			BubbleSort sort = new BubbleSort();
+			sort.sort(str1);
+			sort.sort(str2);
+			System.out.println("Sorted String1: " + Arrays.toString(str1));
+			System.out.println("Sorted String2: " + Arrays.toString(str2));
+			
+			
+			//check if elements are same
+			int length = str1.length;
+			for(int i=0; i< length; i++){
+				if(str1[i].compareTo(str2[i]) != 0){
+					System.out.println("Two strings are not anagrams");
+					return;
+				}
+			}
+			
+			System.out.println("Two strings are anagrams");
+			return;
+			
+		
+			
+		}
+		public String describe(){
+			return "First check if two strings are of same length or not. If they, sort the two strings and compare them";
+			
+		}
+		public String timeComplexity(){
+			return "O(N * Log N)";
+		}
+		public String spaceComplexity(){
+			return "O(1)";
+		}
+	}
+}
---
+++ com/problems/Problem4.java
@@ -0,0 +1,94 @@
+package com.problems;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import com.algo.sort.BubbleSort;
+import java.util.Arrays;
+
+public class Problem4 extends Problem{
+	public Problem4(){
+		super();
+		this.solutions.add(new Solution1());
+	}
+
+	public String question(){
+		return "Write a function to get all possible subsets (Powerset problem). Number of subsets will be equal to 2^n where n is the number of elements";
+	}
+	
+	public Map readParameters() throws Exception{
+		Map<String, Object> options = new HashMap<String, Object>();
+		ArrayList<Comparable> items = new ArrayList<Comparable();
+		
+		InputStreamReader isr = new InputStreamReader(System.in);
+		BufferedReader reader = new BufferedReader(isr);
+		final String quit = "q"
+		while(true){
+			System.out.print("Enter element (press q if done): ");
+			String item = reader.readLine();
+			if(quit.equalsIgnoreCase(item)) break;
+			items.add(item);
+		}
+		
+		options.put("array", items);
+		return options;
+		
+	}
+	
+	public class Solution1 implements Solution{
+		
+		public void execute(Map options){
+			ArrayList<Comparable> items = (ArrayList<Comparable>) options.get("array");
+			
+			//Sanity Check
+			int n = items.size();
+			if(n >= 64){
+				System.out.println("Error: Array length greater than 64")
+				return;
+			}
+			
+			
+			for(long i=0; i < (1<<n); i++){ // run from 0 to 2^n 
+				ArrayList<Comparable> subset = new ArrayList<Comparable>();
+				for(int j=0; j < n; j++){     //search through n bits
+					if((i>>j) & 1) == 1){       //left shift i by j and check first bit 
+						subset.add(itmes.get(j)); // if on then store it
+					}
+				}
+				System.out.println(Arrays.toString(subset));
+			}
+			
+			/* Simpler Representation
+			int powerElements = Math.pow(2, n);
+			for(int i=0; i < powerElements; i++){
+			
+				//conver the binary number to a string containing n digits
+				String binary = intToBinary(i, n);
+				ArrayList<Comparable> subset = new ArrayList<Comparable>();
+				
+				for(int j=0; j < binary.length(); j++){
+					if(binary.charAt(j) == '1') subset.add(items.get(j));
+				}
+				
+			}
+			*/
+			
+			return;
+			
+			
+		}
+		
+		public String describe(){
+			return "Uses BitMasking technique to generate all possible subsets. Incrment counter by 1 at each iterature and select elements for which bit value is 1"
+		}
+
+		public String timeComplexity(){
+			return "polynomical time - O(2^n)";
+		}
+		public String spaceComplexity(){
+			return "2^n";
+		}
+	}
+}
